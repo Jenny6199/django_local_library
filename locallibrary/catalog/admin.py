abdb158a44e831a7fb53942df299a8734135e6d0
Jenny6199 <Jenny6199@yandex.ru>
2022-01-03 12:58:16 +0300

    add empty class Author-, Book-, BookInstance- Admin in catalog/admin.py

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,8 +3,25 @@ from .models import Book, Author, Genre, BookInstance, Language
 
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+# admin.site.register(Book)
+# admin.site.register(Author)
+# admin.site.register(Author, AuthorAdmin)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
 admin.site.register(Language)
